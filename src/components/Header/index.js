de5a26d305f48422826b528c382ec52bbf514b6f
Igor Trindade <igorhideki@gmail.com>
2019-10-11 18:20:13 -0400

    add funcionalidade ao botao sair do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { Container, Content, Profile } from './styles';
 import { Button } from '~/styles/button';
 import logo from '~/assets/logo.svg';
 
 export default function Header() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
@@ -23,7 +29,9 @@ export default function Header() {
             <strong>{profile.name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </Profile>
-          <Button danger>Sair</Button>
+          <Button danger onClick={handleSignOut}>
+            Sair
+          </Button>
         </aside>
       </Content>
     </Container>
