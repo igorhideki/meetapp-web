c574397cfda57de94a75bb56f8f6a94871cb0e5b
Igor Trindade <igorhideki@gmail.com>
2019-10-11 18:19:57 -0400

    add pagina de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function SignIn() {
-  return <div>Profile</div>;
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { FormWrapper } from '~/styles/formWrapper';
+import { Button } from '~/styles/button';
+
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <FormWrapper>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <Button type="submit">Salvar perfil</Button>
+      </Form>
+    </FormWrapper>
+  );
 }
